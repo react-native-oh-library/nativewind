5eef2766b9c7d62d231d1ade78a3a00523b68eb0
TiHxdvt <99526935+TiHxdvt@users.noreply.github.com>
2024-07-15 16:50:18 +0800

    feat: add nativewind harmony (#2)

--- packages/nativewind/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "jest-expo",
-  roots: ["src"],
-  setupFiles: ["react-native-css-interop/testing-library/setup"],
-  setupFilesAfterEnv: [
-    "react-native-css-interop/testing-library/setupAfterEnv",
-  ],
-};
--- packages/nativewind/src/tailwind/index.ts
+++
@@ -1,6 +0,0 @@
-module.exports = () =>
-  // This will be undefined on most platforms
-  process.env.NATIVEWIND_NATIVE === undefined ||
-  process.env.NATIVEWIND_NATIVE === "web"
-    ? require("./web").default
-    : require("./native").default;
